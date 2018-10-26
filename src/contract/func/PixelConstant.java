b6ae79d6a91a2807f24c415d9c6e5109fb150451
ad19900913 <ad19900913@outlook.com>
2018-10-26 08:38:10 +0800

    Initial commit

---
+++ src/contract/func/PixelConstant.java
@@ -0,0 +1,10 @@
+package contract.func;
+
+import contract.util.Nuls;
+
+public interface PixelConstant {
+    Nuls INIT_PRICE = Nuls.ONE;
+    short SIZE = 125;
+    byte START = 1;
+    byte STOP = 2;
+}
